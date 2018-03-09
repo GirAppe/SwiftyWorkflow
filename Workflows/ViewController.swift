8762b4ad5a8d537191afd68de7b2357dd9d31ed9
Andrzej Michnia <amichnia@gmail.com>
2018-03-09 23:20:50 +0100

    Initial Commit

---
+++ Workflows/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Workflows
+//
+//  Created by Andrzej Michnia on 09.03.2018.
+//  Copyright © 2018 Andrzej Michnia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
