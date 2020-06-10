82b21f613b7add8e54aa028411254d2654d11fd1
Andrzej Michnia <amichnia@google.com>
2020-06-10 14:33:34 +0200

    Removed dynamic.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
     products: [
         .library(
             name: "SwiftyWorkflow",
-            type: .dynamic,
             targets: ["SwiftyWorkflow"]
         ),
     ],
